8c333dcfba40cde61f8b72560919ae0cd2710400
Markus Wernersbach <wernersbacher@gmail.com>
2022-05-20 11:57:45 +0200

    notabschaltung hinzugefügt

--- scripts/main.py
+++ scripts/main.py
@@ -1,10 +1,11 @@
 import rospy
 from ros_pi_pwm.msg import PWMArray, PWM
 from geometry_msgs.msg import Twist
+from std_msgs.msg import Bool
 import time
 
 FREQ = 50
-
+RATE = 10  # update
 
 class ServoConvert:
 	# range is 5-10% (1-2ms)
@@ -24,7 +25,7 @@ class ServoConvert:
 		# --- value is in [-1, 1]
 		self.value = value_in
 		self.value_out = self._dir * value_in * self._half_range + self._center
-		rospy.loginfo(f"IN: {self.id}, OUT: {self.value_out}")
+		rospy.logdebug(f"IN: {self.id}, OUT: {self.value_out}")
 		return self.value_out
 
 
@@ -46,13 +47,33 @@ class DkLowLevelCtrl:
 
 		# --- Create the Subscriber to Twist commands
 		self.ros_sub_twist = rospy.Subscriber("/cmd_vel", Twist, self.set_actuators_from_cmdvel)
-		rospy.loginfo("> Subscriber corrrectly initialized")
+		rospy.loginfo("> Subscriber /cmd_vel corrrectly initialized")
 
-		# --- Get the last time e got a commands
+		# --- Get the last time we got a command
 		self._last_time_cmd_rcv = time.time()
-		self._timeout_s = 5
+		self._timeout_cmd = 10
+
+		# --- Create the Subscriber to lifeping
+		self.ros_sub_twist = rospy.Subscriber("/emergency_stop", Bool, self.check_emergency_state)
+		self._last_time_emergency_rcv = time.time()
+		self._last_emergency_msg = True  # no driving allowed until we have positive message from /emergency_stop
+		self._timeout_emergency = 1  # after 1 second message timeout, bring the car to a stop
+		rospy.loginfo("> Subscriber /emergency_stop corrrectly initialized")
 
 		rospy.loginfo("Initialization complete")
+
+	def check_emergency_state(self, stop_required):
+		self._last_emergency_msg = stop_required.data
+		self._last_time_emergency_rcv = time.time()
+
+	@property
+	def is_emergency_stop(self):
+		rospy.logdebug(time.time() - self._last_time_emergency_rcv)
+		# if emergency stop is set to true or the last message is too long ago, we set the state to TRUE
+		state = self._last_emergency_msg or time.time() - self._last_time_emergency_rcv > self._timeout_emergency
+		if state:
+			rospy.logwarn(" !!!! ------ Emergency STOP  ------ !!!! ")
+		return state
 
 	def set_actuators_from_cmdvel(self, message):
 		"""
@@ -90,23 +111,23 @@ class DkLowLevelCtrl:
 			self._servo_msg.pwms[servo_obj.id - 1].pwm_num = servo_obj.id
 			self._servo_msg.pwms[servo_obj.id - 1].freq = FREQ
 			self._servo_msg.pwms[servo_obj.id - 1].duty = servo_obj.value_out
-			rospy.loginfo("Sending to %s command %d" % (actuator_name, servo_obj.value_out))
+			rospy.loginfo("Sending to %s command %f" % (actuator_name, servo_obj.value_out))
 
 		self.ros_pub_servo_array.publish(self._servo_msg)
 
 	@property
 	def is_controller_connected(self):
-		rospy.loginfo(time.time() - self._last_time_cmd_rcv)
-		return time.time() - self._last_time_cmd_rcv < self._timeout_s
+		rospy.logdebug(time.time() - self._last_time_cmd_rcv)
+		return time.time() - self._last_time_cmd_rcv < self._timeout_cmd
 
 	def run(self):
 
 		# --- Set the control rate
-		rate = rospy.Rate(10)
+		rate = rospy.Rate(RATE)
 
 		while not rospy.is_shutdown():
-			rospy.loginfo(f"Last time: {self._last_time_cmd_rcv}, Connected = {self.is_controller_connected}")
-			if not self.is_controller_connected:
+			rospy.loginfo(f"Emergency Stop = {self.is_emergency_stop}, Connected = {self.is_controller_connected}")
+			if self.is_emergency_stop or not self.is_controller_connected:
 				self.set_actuators_idle()
 
 			rate.sleep()
